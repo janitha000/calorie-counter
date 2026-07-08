dc9a059a876473cba60df717d8e8747be1da837b
Janitha Tennakoon <janitha@randomsoftware.net>
2026-07-08 08:57:33 +0530

    fix: use Prisma 7 driver adapter for Postgres connection

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,17 @@
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
+const connectionString = process.env.DATABASE_URL || ''
+
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  if (!connectionString) {
+    // Return a dummy client if no DB URL is present during build time
+    return new PrismaClient()
+  }
+  const pool = new Pool({ connectionString })
+  const adapter = new PrismaPg(pool)
+  return new PrismaClient({ adapter })
 }
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
