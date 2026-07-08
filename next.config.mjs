449235e46f1555c92fb8824c909474d51985d411
Janitha Tennakoon <janitha@randomsoftware.net>
2026-07-08 08:45:04 +0530

    fix: turbopack config for next-pwa build error

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const withPWA = withPWAInit({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  turbopack: {},
 };
 
 export default withPWA(nextConfig);
